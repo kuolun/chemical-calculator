9efa090a884ef07b20966dc68fb4734bdc6fd068
kuolun <kuolun@gmail.com>
2019-02-04 00:39:40 +0800

    fix layout and v-model

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        input: 1
-    },
-    computed: {
-        answer() {
-            return -Math.log10(this.input);
-        }
+  el: '#app',
+  data: {
+    input: {
+      option: '請選擇',
+      value: 1
     }
-});
+  },
+  computed: {
+    answer() {
+      return -Math.log10(this.input.value).toFixed(2)
+    }
+  }
+})
