0147958bc4152e30c447a0f5aff31f108af0b93b
kuolun <kuolun@gmail.com>
2019-02-11 22:37:20 +0800

    v1 complete

--- app.js
+++ app.js
@@ -46,16 +46,47 @@ let data = {
       ka: 1.8 * Math.pow(10, -5)
     }
   ],
-  selectedKa: 0
+  selectedKa: 0,
+  productSq: 0,
+  BlessthanC5: false
 }
 
 var app = new Vue({
   el: '#app',
   data: data,
   computed: {
-    answer() {
+    answer1() {
+      console.log('answer1');
       // return -Math.log10(this.input.value).toFixed(2)
-      return 0
+      this.productSq = Math.sqrt(this.inputValue * this.selectedKa);
+      return this.productSq;
+    },
+    answer2() {
+      if (this.productSq > this.inputValue * 0.05) {
+        this.BlessthanC5 = false;
+        let x1 = (-this.selectedKa + Math.sqrt(Math.pow(this.selectedKa, 2) + 4 * this.selectedKa * this.inputValue)) / 2;
+        let x2 = (-this.selectedKa - Math.sqrt(Math.pow(this.selectedKa, 2) + 4 * this.selectedKa * this.inputValue)) / 2;
+        console.log(x1, x2);
+        let finxalX;
+        if (x1 > 0) {
+          finalX = x1
+        } else {
+          finalX = x2
+        }
+        let pH1 = -Math.log10(finalX).toFixed(2);
+        if (pH1 > 7) {
+          pH1 = 7
+        }
+        return pH1;
+      } else {
+        this.BlessthanC5 = true;
+        console.log('in pH2');
+        let pH2 = -Math.log10(this.productSq);
+        if (pH2 > 7) {
+          pH2 = 7
+        }
+        return pH2;
+      }
     }
   }
 })
