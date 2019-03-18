8f8e70060bde887d7b9be30c0660ef674cf2fd69
kuolun <kuolun@gmail.com>
2019-03-18 16:58:47 +0800

    case3 prototype done

--- case3/app.js
+++ case3/app.js
@@ -1,148 +1,31 @@
 let data = {
-  selectel1: {},
-  selectel2: {},
-  elements1: [
+  select1: {},
+  select2: {},
+  acid_concentration: 0,
+  acid_volume: 0,
+  acids: [
     {
-      name: "Li",
-      text: "鋰 Lithium (Li)",
-      option: 1//保留正離子
+      name: "HI(hydroiodic acid)",
+      ka: Math.pow(10, 10),
+      pka: -10
     },
     {
-      name: "Na",
-      text: "鈉 Sodium (Na)",
-      option: 1//保留正離子
-    },
-    {
-      name: "K",
-      text: "鉀 Potassium (K)",
-      option: 1//保留正離子
-    },
-    {
-      name: "Rb",
-      text: "銣 Rubidium (Rb)",
-      option: 1//保留正離子
-    },
-    {
-      name: "Cs",
-      text: "銫 Caesium (Cs)",
-      option: 1//保留正離子
-    },
-    {
-      name: "Cs",
-      text: "銫 Caesium (Cs)",
-      option: 1//保留正離子
-    },
-    {
-      name: "Be",
-      text: "鈹 Berylium (Be)",
-      option: 1//保留正離子
-    },
-    {
-      name: "Mg",
-      text: "鎂 Magnesium (Mg)",
-      option: 1//保留正離子
-    },
-    {
-      name: "Ca",
-      text: "鈣 Calcium (Ca)",
-      option: 1//保留正離子
-    },
-    {
-      name: "Sr",
-      text: "鍶 Strontium (Sr)",
-      option: 1//保留正離子
-    },
-    {
-      name: "Ba",
-      text: "鋇 Barium (Ba)",
-      option: 1//保留正離子
-    },
-  ],
-  elements2: [
-    {
-      name: "OH",
-      text: ' 氫氧根 Hydroxide (OH)',
-      option: 1
+      name: "HBr(hydrobromic acid)",
+      ka: Math.pow(10, 9),
+      pka: -9
     }
   ],
-  // OH沈澱組合
-  oh_down: [
+  // 鹼
+  alka: [
     {
-      name: 'Li',
-      down: false,
-      result: 'LiOH'
-    },
-    {
-      name: 'Na',
-      down: false,
-      result: 'NaOH'
-    },
-    {
-      name: 'K',
-      down: false,
-      result: 'KOH'
-    },
-    {
-      name: 'Rb',
-      down: false,
-      result: 'RbOH'
-    },
-    {
-      name: 'Cs',
-      down: false,
-      result: 'CsOH'
-    },
-    {
-      name: 'Be',
-      down: true,
-      result: 'Be(OH)2'
-    },
-    {
-      name: 'Mg',
-      down: true,
-      result: 'Mg(OH)2'
-    },
-    {
-      name: 'Ca',
-      down: true,
-      result: 'Ca(OH)2'
-    },
-    {
-      name: 'Sr',
-      down: true,
-      result: 'Sr(OH)2'
-    },
-    {
-      name: 'Ba',
-      down: false,
-      result: 'Ba(OH)2'
-    },
-    {
-      name: 'Fe',
-      down: true,
-      result: 'Fe(OH)3'
-    },
-    {
-      name: 'Cu',
-      down: true,
-      result: 'Cu(OH)2'
-    },
-    {
-      name: 'Ag',
-      down: true,
-      result: 'AgOH'
-    },
-    {
-      name: 'Hg',
-      down: true,
-      result: 'Hg(OH)2'
-    },
-    {
-      name: 'Pb',
-      down: true,
-      result: 'Pb(OH)2'
-    },
-  ]
+      name: "OH氫氧根 Hyd  roxide (OH)",
+      kb: 1,
+      pkb: 0
+    }
+  ],
+  alka_volume: 0,
+  alka_concentration: 0,
+  pH: 0
 };
 
 var app = new Vue({
@@ -167,16 +50,52 @@ var app = new Vue({
   },
   computed: {
     result() {
-      const result = [];
-      const down = this.oh_down;
-      const len = this.oh_down.length;
-      for (let i = 0; i < len; i++) {
-        if (this.selectel1.name === down[i].name) {
-          console.log('test')
-          result.push(down[i]);
-        }
+      const acid_compute = this.acid_volume * this.acid_concentration;
+      const alka_compute = this.alka_volume * this.alka_concentration;
+
+      let pH = this.pH;
+      console.log(pH);
+
+      // case1
+      if (acid_compute > alka_compute) {
+        console.log("case1");
+        const e = acid_compute - alka_compute;
+        pH = this.select1.pka + Math.log10(e / alka_compute);
       }
-      return result;
+
+      // case2
+      if (acid_compute === alka_compute) {
+        console.log("case2");
+        const f = acid_compute;
+        const g = f / (this.acid_volume + this.alka_volume);
+        const kb = this.select2.kb;
+
+        console.log(`f:${f},g:${g},kb:${kb}`);
+
+        const h1 = (-kb + Math.sqrt(Math.pow(kb, 2) + 4 * kb * g)) / 2;
+        const h2 = (-kb - Math.sqrt(Math.pow(kb, 2) + 4 * kb * g)) / 2;
+        console.log(`h1:${h1}`, `h2:${h2}`);
+        if (h1 > 0) {
+          pH = Math.log10(h1);
+        }
+        // h2也>0??? 待確認
+      }
+
+      // case3
+      if (acid_compute < alka_compute) {
+        const i = alka_compute - acid_compute;
+        const j = i / (acid_volume + alka_volume);
+        pH = 14 + Math.log10(j);
+      }
+
+      // 檢查pH範圍是否在1~14之間
+      if (pH < 1) {
+        pH = 1;
+      }
+      if (pH > 14) {
+        pH = 14;
+      }
+      return pH;
     }
   }
 });
