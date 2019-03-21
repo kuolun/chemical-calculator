13dcb8ad65f13ebf4a71494ee39efad51412cd04
kuolun <kuolun@gmail.com>
2019-03-21 13:37:49 +0800

    add all acid data

--- case3/app.js
+++ case3/app.js
@@ -13,6 +13,71 @@ let data = {
       name: "HBr(hydrobromic acid)",
       ka: Math.pow(10, 9),
       pka: -9
+    },
+    {
+      name: "HCl(hydrochloric acid)",
+      ka: Math.pow(10, 6),
+      pka: -6
+    },
+    {
+      name: "H2SO4(sulfuric acid)",
+      ka: Math.pow(10, 3),
+      pka: -3
+    },
+    {
+      name: "HNO3(nitric acid)",
+      ka: Math.pow(10, 2),
+      pka: -2
+    },
+    {
+      name: "H3O+(hydronium acid)",
+      ka: 1,
+      pka: 0
+    },
+    {
+      name: "HSO4-(hydrogen sulfate ion)",
+      ka: Math.pow(10, -2) * 1.3,
+      pka: 1.89
+    },
+    {
+      name: "HF(hydrofluoric acid)",
+      ka: Math.pow(10, -4) * 7.1,
+      pka: 3.15
+    },
+    {
+      name: "HNO2(nitrous acid)",
+      ka: Math.pow(10, -4) * 4.5,
+      pka: 3.35
+    },
+    {
+      name: "HCOOH(formic acid)",
+      ka: Math.pow(10, -4) * 1.7,
+      pka: 3.77
+    },
+    {
+      name: "CH3COOH(acetic acid)",
+      ka: Math.pow(10, -5) * 1.8,
+      pka: 4.74
+    },
+    {
+      name: "NH4+(ammonium ion)",
+      ka: Math.pow(10, -10) * 5.6,
+      pka: 9.25
+    },
+    {
+      name: "HCN(hydrocyanic acid)",
+      ka: Math.pow(10, -10) * 4.9,
+      pka: 9.31
+    },
+    {
+      name: "H2O(water)",
+      ka: Math.pow(10, -14),
+      pka: 14
+    },
+    {
+      name: "NH3(ammonia)",
+      ka: Math.pow(10, -34),
+      pka: 34
     }
   ],
   // 鹼
